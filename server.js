053f20816d3c307dd0ebb73bd5bd722b9d5fd81e
Jorge Alejandro <alejoved@gmail.com>
2020-05-29 20:39:32 -0500

    modificacion en package 2

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-var express = require('express');
-var app = express();
-app.use(express.static('./dist/sic-ecuador-frontend'));
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/sic-ecuador-frontend'));
+
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', { root: 'dist/sic-ecuador-frontend/' });
+
+    res.sendFile(path.join(__dirname + '/dist/sic-ecuador-frontend/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
